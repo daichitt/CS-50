e1c3d94e0102e63a00cfe9f591adcb3352f3bec7
daichitt <gene.sixteen.000925@outlook.jp>
2023-01-01 19:59:54 +0900

    add condition.py

---
+++ sample-code/condition.py
@@ -0,0 +1,27 @@
+# # myVariable = input('Enter a number')
+
+# myVariable = 1
+# # myint = 7
+
+
+# if type(myVariable) == int or type(myVariable) == float:
+#     print(type(myVariable))
+#     # print('The variable is integer or float')
+#     # Do something
+# else:
+#     print('The variable is not a number')
+
+# # myVariable = 'A string'
+# # if type(myVariable) == int or float:
+# #     print('The variable a number')
+# # else:
+# #     print('The variable is not a number')
+
+n = int(input("Number:"))
+
+if n > 0:
+    print(" n is positive")
+elif n < 0:
+    print(" n is nrgative")
+else:
+    print(" n is zero")
